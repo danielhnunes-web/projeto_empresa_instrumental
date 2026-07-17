293e9eb83157dbb5d6228065be8d3aefe21674b7
Daniel <daniel.h.nunes@aluno.senai.br>
2026-07-17 14:51:46 -0300

    termeinei a função para buscar o cep do usuário e prencher as outras informações do local onde ele mora automaticamente

--- js/cadastro.js
+++ js/cadastro.js
@@ -28,3 +28,35 @@ function cadastrar(){
 
     window.location.href = "login.html";
 }
+
+async function buscarCEP() {
+    const cep = document.getElementById("cep").value.replace(/\D/g, "");
+
+    if (cep.length !== 8) {
+        alert("Digite um CEP válido.");
+        return;
+    }
+
+    try{
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+        const dados = await resposta.json();
+
+        if (dados.erro) {
+            alert("CEP não encontrado.");
+            return;
+        }
+
+        document.getElementById("logradouro").value = dados.logradouro;
+        document.getElementById("bairro").value = dados.bairro;
+        document.getElementById("cidade").value = dados.localidade;
+        document.getElementById("estado").value = dados.uf;
+
+    } catch (erro) {
+        alert("Erro ao consultar o CEP.")
+    }
+}
+
+window.onload = function(){
+    document.getElementById("cep").addEventListener("blur", buscarCEP);
+}
